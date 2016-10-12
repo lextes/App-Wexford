6153baafeeb9b7cc04a0e8e90446b47ccdc31774
Diseño <diseno@macbook-air-de-diseno.local>
2016-10-11 21:59:00 -0500

    Initial Commit

---
+++ App Wex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  App Wex
+//
+//  Created by Diseño on 11/10/16.
+//  Copyright © 2016 Desarrollo WEB Wexford. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ App WexTests/App_WexTests.swift
@@ -0,0 +1,36 @@
+//
+//  App_WexTests.swift
+//  App WexTests
+//
+//  Created by Diseño on 11/10/16.
+//  Copyright © 2016 Desarrollo WEB Wexford. All rights reserved.
+//
+
+import XCTest
+@testable import App_Wex
+
+class App_WexTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
